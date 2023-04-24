4de55cced215001a0c5593b86c8c6eee0cfc8d86
Robert Petersen <robert.petersen808@gmail.com>
2023-04-24 14:53:42 -0500

    created routes

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,18 @@
 import './App.css'
 import Nav from "./components/Nav";
 import Footer from "./components/Footer";
-// import Landing from "./components/Landing";
-// import Results from "./components/Results";
+import Landing from "./components/Landing";
+import Results from "./components/Results";
+import { Routes, Route } from "react-router-dom"
 
 function App() {
   return (
     <>
       <Nav />
-
-
-
+      <Routes>
+        <Route path="/" element={<Landing />}/>
+        <Route path="/forcast" element={<Results />}/>
+      </Routes>
       <Footer />
     </>
   )
