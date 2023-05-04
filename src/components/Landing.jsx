75aac371dba6643036fdc527a4e452d9149011ec
Robert Petersen <robert.petersen808@gmail.com>
2023-05-03 20:37:33 -0500

    layed out content for the landing page

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -1,10 +1,29 @@
 // import React from 'react'
+import Search from "./Search"
 
-const Landing = () => {
+const Landing = ({ setResults }) => {
   return (
-    <div>
-
-    </div>
+    <main>
+      <section>
+        <h1>WeatherNow</h1>
+        <p>Search for weather information from any city in the world!</p>
+        <Search setResults={setResults}/>
+      </section>
+      <section className="carousel">
+        <article className="current">
+          <h1>Current Weather</h1>
+          <p>Get a summary of the most relevant current weather information for the city you search!</p>
+        </article>
+        <article className="forcasts">
+          <h1>Forcast Info</h1>
+          <p>Recive a detailed forcast with visuals for the current day and next two days for any city!</p>
+        </article>
+        <article className="astronomy">
+          <h1>Astronomy Info</h1>
+          <p>Recive information on astronomical infromation such as sunrises, sunsets, and moon phases.</p>
+        </article>
+      </section>
+    </main>
   )
 }
 
